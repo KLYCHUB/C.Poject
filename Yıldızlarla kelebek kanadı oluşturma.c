b79e2641891dec4eb2a6c1284e6649d375cbbc19
Eren KALAYCI <94598244+KLYCHUB@users.noreply.github.com>
2021-12-07 21:26:33 +0300

    Create Yıldızlarla kelebek kanadı oluşturma.c

---
+++ Yıldızlarla kelebek kanadı oluşturma.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	int i,j;
+
+	for(i=1 ; i<=5 ; i++)
+	{
+	
+		for(j=1 ; j<=i ; j++)
+		{
+			printf("*");
+		}
+		for(j=1 ; j<=5-i ; j++)
+		{
+			printf(" ");
+		}
+		for(j=1 ; j<=5-i ; j++)
+		{
+			printf(" ");
+		}
+		for(j=1 ; j<=i ; j++)
+		{
+			printf("*");
+		}
+		printf("\n");	
+	
+	}
+	
+	//Alt yari
+	
+	for(i=5 ; i>=1 ; i--)
+	{        
+		
+		for(j=1 ; j<=i ; j++)
+		{
+			printf("*");	
+		}
+		for(j=1 ; j<=5-i ; j++)
+		{
+			printf(" ");
+		}
+		for(j=1 ; j<=5-i ; j++)
+		{
+			printf(" ");
+		}
+		for(j=1 ; j<=i ; j++)
+		{
+			printf("*");	
+		}
+		
+		printf("\n");
+		
+	}
+	
+	return 0;
+}
