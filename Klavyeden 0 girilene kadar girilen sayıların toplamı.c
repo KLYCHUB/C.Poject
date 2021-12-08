adf49645af1815a1d4d2856acfaf06c5eea1dc49
Eren KALAYCI <94598244+KLYCHUB@users.noreply.github.com>
2021-12-08 23:41:10 +0300

    Create Klavyeden 0 girilene kadar girilen sayıların toplamı.c

---
+++ Klavyeden 0 girilene kadar girilen sayıların toplamı.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ 
+  int sayi,i,sayac;
+	
+	while(sayi!=0)
+	{
+		printf("Sayi giriniz: ");
+		scanf("%d",&sayi);
+		
+		sayac = sayac + sayi;
+	}
+
+  return 0;  
+}
