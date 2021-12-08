0b73a72dbb07b03e43341f3b2f591cb493ab41af
Eren KALAYCI <94598244+KLYCHUB@users.noreply.github.com>
2021-12-08 22:02:01 +0300

    Create Her saat başı kendini 2' ye bölen bir bakteri 24 saat sonra sayısı kaç olur.c

---
+++ Her saat başı kendini 2' ye bölen bir bakteri 24 saat sonra sayısı kaç olur.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	
+	int i,j;
+	j = 1;
+	
+	for(i=1;i<=24;i++)
+	{
+		
+		j = j*2;
+		
+	}
+	
+	printf("%d Tane bakteri olur...",j);
+	
+	
+	return 0;
+}
