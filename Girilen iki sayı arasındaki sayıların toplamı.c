2fa7455424397d86f05324cd1e958c9e7aef5ba8
Eren KALAYCI <94598244+KLYCHUB@users.noreply.github.com>
2021-12-09 02:02:26 +0300

    Update Girilen iki sayı arasındaki sayıların toplamı.c

--- Girilen iki sayı arasındaki sayıların toplamı.c
+++ Girilen iki sayı arasındaki sayıların toplamı.c
@@ -3,35 +3,41 @@
 
 int main()
 {
+
+	int x, y, i;
+	i = 0;
 	
-	int x,y,i;
-	i=0;
-	
+    yeniden:
+    	
 	printf("iki sayi gir: ");
-	scanf("%d %d",&x,&y);
-	
-	if(x!=y || y!=x)
+	scanf_s("%d %d", &x, &y);
+
+	if (x != y)
 	{
-		
-		if(x>y)
+
+		if (x > y)
 		{
-			for(y=y+1;y<x;y++)
+			for (y = y + 1; y < x; y++)
 			{
-				i=i+y;
+				i = i + y;
 			}
-			printf("Sayilarin toplami: %d",i);
+			printf("Sayilarin toplami: %d", i);
 		}
-		else if(x<y)
+		else if (x < y)
 		{
-			for(x=x+1;x<y;x++)
+			for (x = x + 1; x < y; x++)
 			{
-				i=i+x;
+				i = i + x;
 			}
-			printf("Sayilarin toplami: %d",i);
-			
+			printf("Sayilarin toplami: %d", i);
+
 		}
-		
+
 	}
-	
+	else
+	{
+		goto yeniden;
+	}
+
 	return 0;
 }
